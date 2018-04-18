d820a743e9d544bf90689a618459a22e101958fb
Sasa Krajnc <kra40207873@CHIC.local>
2018-04-18 10:22:58 +0100

    Add project files.

---
+++ Attempt1/Attempt1/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Attempt1
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+        private void Button1_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushModalAsync(new TestPage());
+        }
+        private void Button2_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushModalAsync(new RegisterPage());
+        }
+    }
+}
---
+++ Attempt1/Attempt1/TestPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Timers;
+
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Attempt1
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TestPage : TabbedPage
+    {
+        private System.Timers.Timer timer = new Timer();
+        int time;
+        public TestPage()
+        {
+            InitializeComponent();
+            timer = new Timer();
+        }
+        private void Run(object sender, EventArgs e)
+        {
+            timer.Interval = 1;
+            timer.Elapsed += Timer_count;
+            timer.Enabled = true;
+        }
+        private void Timer_count(object sender, EventArgs e)
+        {
+            time++;
+            Numbers.Text = time.ToString();
+        }
+        private void Stop(object sender, EventArgs e)
+        {
+            timer.Enabled = false;
+        }
+    }
+}
